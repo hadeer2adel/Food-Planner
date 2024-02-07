d5b9f45b3fe2f9008ae37494b0770b02eb206d00
Hadeer <hadeeradel502@gmail.com>
2024-02-07 04:48:48 +0200

    add signup layout

--- Food Planner/app/src/main/java/com/example/foodplanner/View/SignupActivity.java
+++ Food Planner/app/src/main/java/com/example/foodplanner/View/SignupActivity.java
@@ -2,18 +2,15 @@ package com.example.foodplanner.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.foodplanner.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_signup);
     }
 }
