9401d3ea163c88dbeb0d24ba26682977afc6756c
Hadeer <hadeeradel502@gmail.com>
2024-02-07 03:12:40 +0200

    add login layout

--- Food Planner/app/src/main/java/com/example/foodplanner/View/MainActivity.java
+++ Food Planner/app/src/main/java/com/example/foodplanner/View/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.foodplanner;
+package com.example.foodplanner.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.foodplanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
